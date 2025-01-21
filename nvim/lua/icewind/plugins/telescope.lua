ce97045ca10dbafb8fe9f868331dba1c1e084ac9
icewind <northicewind@gmail.com>
2025-01-21 20:15:01 +0300

    Added backdrop for the telescope window

--- nvim/lua/icewind/plugins/telescope.lua
+++ nvim/lua/icewind/plugins/telescope.lua
@@ -72,5 +72,50 @@ return {
         vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
 
         vim.keymap.set("n", "<leader>gs", require("telescope.builtin").git_status, { desc = "[G]it [S]tatus" })
+
+        -- Add backdrop to make floating window more contrast
+        -- The code is from https://github.com/nvim-telescope/telescope.nvim/issues/3020#issuecomment-2439446111
+        local blend = 70
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "TelescopePrompt",
+            callback = function(ctx)
+                local backdropName = "TelescopeBackdrop"
+                local telescopeBufnr = ctx.buf
+
+                local telescopeZindex = 50
+
+                local backdropBufnr = vim.api.nvim_create_buf(false, true)
+                local winnr = vim.api.nvim_open_win(backdropBufnr, false, {
+                    relative = "editor",
+                    row = 0,
+                    col = 0,
+                    width = vim.o.columns,
+                    height = vim.o.lines,
+                    focusable = false,
+                    style = "minimal",
+                    zindex = telescopeZindex - 1,
+                })
+
+                vim.api.nvim_set_hl(0, backdropName, { bg = "#000000", default = true })
+                vim.wo[winnr].winhighlight = "Normal:" .. backdropName
+                vim.wo[winnr].winblend = blend
+                vim.bo[backdropBufnr].buftype = "nofile"
+
+                -- close backdrop when the reference buffer is closed
+                vim.api.nvim_create_autocmd({ "WinClosed", "BufLeave" }, {
+                    once = true,
+                    buffer = telescopeBufnr,
+                    callback = function()
+                        if vim.api.nvim_win_is_valid(winnr) then
+                            vim.api.nvim_win_close(winnr, true)
+                        end
+                        if vim.api.nvim_buf_is_valid(backdropBufnr) then
+                            vim.api.nvim_buf_delete(backdropBufnr, { force = true })
+                        end
+                    end,
+                })
+            end,
+        })
     end,
 }
