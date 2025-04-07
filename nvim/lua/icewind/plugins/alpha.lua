0acd8c08f38d7444a345a9712498a9dcfa8cde46
icewind <northicewind@gmail.com>
2025-04-07 22:07:24 +0300

    Cleanup and minor dashboard modification

--- nvim/lua/icewind/plugins/alpha.lua
+++ nvim/lua/icewind/plugins/alpha.lua
@@ -22,14 +22,15 @@ return {
         dashboard.section.buttons.val = {
             dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
             dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-            dashboard.button("SPC sf", "󰱼 > Search File", "<cmd>Telescope find_files<CR>"),
+            dashboard.button("SPC sf", "󰱼  > Search File", "<cmd>Telescope find_files<CR>"),
             dashboard.button(
                 "SPC sg",
                 "  > Find Word",
                 ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>"
             ),
+            dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
             dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-            dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+            dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
         }
 
         -- Send config to alpha
