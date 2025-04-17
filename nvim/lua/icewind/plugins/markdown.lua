51dad72f119e8d49c2168e90bf7b10013c49d637
icewind <northicewind@gmail.com>
2025-04-18 00:30:26 +0300

    Prevent layout shift in markdown file

--- nvim/lua/icewind/plugins/markdown.lua
+++ nvim/lua/icewind/plugins/markdown.lua
@@ -1,7 +1,12 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+    config = function()
+        require("render-markdown").setup({
+            code = {
+                enabled = true,
+                border = "none",
+            },
+        })
+    end,
 }
