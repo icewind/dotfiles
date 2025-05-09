ecec533289b30634803e28eb176806f3c54699d7
icewind <northicewind@gmail.com>
2025-05-09 19:20:03 +0300

    Corrected the notes path

--- nvim/lua/icewind/plugins/floatnotes.lua
+++ nvim/lua/icewind/plugins/floatnotes.lua
@@ -3,7 +3,7 @@ return {
     opts = {},
     config = function()
         require("floatnotes").setup({
-            global_notes_file_path = os.getenv("GLOBAL_NOTES"),
+            global_notes_file_path = os.getenv("GLOBAL_NOTES_PATH"),
         })
         vim.keymap.set("n", "<leader>fn", ":FloatNotes<CR>", { silent = true, noremap = true })
     end,
