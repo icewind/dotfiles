04c6cca6cb812839a32ac661a6686cb16c833e7a
icewind <northicewind@gmail.com>
2025-05-09 19:18:28 +0300

    Added float notes plugin

---
+++ nvim/lua/icewind/plugins/floatnotes.lua
@@ -0,0 +1,10 @@
+return {
+    "icewind/floatnotes.nvim",
+    opts = {},
+    config = function()
+        require("floatnotes").setup({
+            global_notes_file_path = os.getenv("GLOBAL_NOTES"),
+        })
+        vim.keymap.set("n", "<leader>fn", ":FloatNotes<CR>", { silent = true, noremap = true })
+    end,
+}
