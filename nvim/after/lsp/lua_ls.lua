6a6f7036bf735c4b88a9ce57c15c308fe8bfb489
icewind <northicewind@gmail.com>
2025-05-10 20:45:42 +0300

    Some lua ls settings

--- nvim/after/lsp/lua_ls.lua
+++ nvim/after/lsp/lua_ls.lua
@@ -1,16 +1,26 @@
 return {
-  settings = {
-    telemetry = { enable = false },
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = {
-          'vim',
-          'require',
+    settings = {
+        telemetry = { enable = false },
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+                path = {
+                    'lua/?.lua',
+                    'lua/?/init.lua',
+                },
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                }
+            },
+            diagnostics = {
+                globals = {
+                    'vim',
+                    'require',
+                },
+            },
         },
-      },
     },
-  },
 }
