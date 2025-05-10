9935d8de23d4870ba44576a1575e40b08a633cc5
icewind <northicewind@gmail.com>
2025-05-10 14:22:53 +0300

    Using rounded border for floating notes window

--- nvim/lua/icewind/plugins/floatnotes.lua
+++ nvim/lua/icewind/plugins/floatnotes.lua
@@ -5,6 +5,9 @@ return {
     config = function()
         require("floatnotes").setup({
             global_notes_file_path = os.getenv("GLOBAL_NOTES_PATH"),
+            win = {
+                border = "rounded"
+            }
         })
         vim.keymap.set("n", "<leader>fn", ":FloatNotes<CR>", { silent = true, noremap = true })
     end,
