520f6c186524c9d53c45f0e7523565bd92d4826d
icewind <northicewind@gmail.com>
2025-11-25 22:38:25 +0300

    fix(nvim): adjusted kanso theme color for split borders

--- nvim/lua/icewind/plugins/colorschemes/kanso.lua
+++ nvim/lua/icewind/plugins/colorschemes/kanso.lua
@@ -2,4 +2,15 @@ return {
     "webhooked/kanso.nvim",
     lazy = false,
     priority = 1000,
+    opts = {
+        bold = false,
+        background = {
+            dark = "mist",
+        },
+        overrides = function(colors)
+            return {
+                WinSeparator = { fg = colors.palette.gray5 },
+            }
+        end,
+    },
 }
