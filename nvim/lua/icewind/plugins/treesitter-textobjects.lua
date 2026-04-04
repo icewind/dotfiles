573dded2bd64243414a3cb7ad9b196f3ed1910e8
icewind <northicewind@gmail.com>
2026-04-04 19:08:11 +0300

    fix: updated treesitter for 0.12 nvim

---
+++ nvim/lua/icewind/plugins/treesitter-textobjects.lua
@@ -0,0 +1,53 @@
+return {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+        vim.g.no_plugin_maps = true
+    end,
+    config = function()
+        return {
+            select = {
+                enable = true,
+                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ["aa"] = "@parameter.outer",
+                    ["ia"] = "@parameter.inner",
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                },
+            },
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
+                },
+            },
+            swap = {
+                enable = true,
+                swap_next = {
+                    ["<leader>a"] = "@parameter.inner",
+                },
+                swap_previous = {
+                    ["<leader>A"] = "@parameter.inner",
+                },
+            },
+        }
+    end,
+}
--- nvim/lua/icewind/plugins/treesitter.lua
+++ nvim/lua/icewind/plugins/treesitter.lua
@@ -1,16 +1,17 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPre", "BufNewFile" },
+    branch = "main",
     build = ":TSUpdate",
+    lazy = false,
     dependencies = {
         "windwp/nvim-ts-autotag",
-        "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
         ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup({
+        require("nvim-treesitter.config").setup({
             -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = { "go", "lua", "python", "rust", "typescript" },
+            ensure_installed = { "go", "lua", "python", "rust", "typescript", "markdown" },
 
             autotag = {
                 enable = true,
@@ -25,50 +26,6 @@ return {
                     node_incremental = "<c-space>",
                     scope_incremental = "<c-s>",
                     node_decremental = "<c-backspace>",
-                },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                    keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
-                        ["aa"] = "@parameter.outer",
-                        ["ia"] = "@parameter.inner",
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                    },
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true, -- whether to set jumps in the jumplist
-                    goto_next_start = {
-                        ["]m"] = "@function.outer",
-                        ["]]"] = "@class.outer",
-                    },
-                    goto_next_end = {
-                        ["]M"] = "@function.outer",
-                        ["]["] = "@class.outer",
-                    },
-                    goto_previous_start = {
-                        ["[m"] = "@function.outer",
-                        ["[["] = "@class.outer",
-                    },
-                    goto_previous_end = {
-                        ["[M"] = "@function.outer",
-                        ["[]"] = "@class.outer",
-                    },
-                },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ["<leader>a"] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        ["<leader>A"] = "@parameter.inner",
-                    },
                 },
             },
         })
