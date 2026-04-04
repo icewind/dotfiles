8d296e54e61e81079bb903c24ea6a1015cc3a110
icewind <northicewind@gmail.com>
2026-04-04 15:19:33 +0300

    fix: ts_ls config format

--- nvim/lsp/ts_ls.lua
+++ nvim/lsp/ts_ls.lua
@@ -1,11 +1,8 @@
-return function(utils)
-    return {
-        init_options = {
-            preferences = {
-                disableSuggestions = true,
-            },
+return {
+    init_options = {
+        preferences = {
+            disableSuggestions = true,
         },
-        root_dir = utils.root_pattern("project.json"),
-        -- single_file_support = false,
-    }
-end
+    },
+    single_file_support = false,
+}
